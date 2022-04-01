8b30d9e2be5a2a4f8d41f2eaca8e448eb3b546c1
Kevin Kendle <1066413+KKendle@users.noreply.github.com>
2022-03-31 20:25:09 -0500

    add data file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp');
 const nunjucksRender = require('gulp-nunjucks-render');
 const sass = require('gulp-sass')(require('sass'));
+const data = require('gulp-data');
 
 const nunjucksHtml = (done) => {
   gulp.src('src/pages/*.html')
+    .pipe(data(() => {
+      return require('./src/data/pageData.json');
+    }))
     .pipe(
       nunjucksRender({
         path: ['src/templates']
